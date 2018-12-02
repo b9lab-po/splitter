67b3dd6b267c6f2af17aee11dc59ecffab26f8e8
Patryk Okuniewicz <patryk.okuniewicz@espeo.eu>
2018-12-02 03:26:04 +0100

    Add CR fixes

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,61 +2,42 @@ pragma solidity ^0.4.24;
 
 contract Splitter {
     address public owner;
+    address public firstPerson;
+    address public secondPerson;
 
-    struct People {
-        address person;
-        uint256 balance;
-    }
-
-    People[2] public people;
+    mapping(address => uint) public balances;
     
     event LogOwner(address indexed owner);
-    event LogSplit(address person, uint amount);
+    event LogSplit(address owner, address firstPerson, address secondPerson, uint amount);
     event LogWithdraw(address person, uint amount);
     
     constructor (address firstPerson, address secondPerson) public {
+        require(firstPerson != address(0), "The address is not set");
+        require(secondPerson != address(0), "The address is not set");
         owner = msg.sender;
         emit LogOwner(owner);
 
-        people[0].person = firstPerson;
-        people[0].balance = 0;
-        people[1].person = secondPerson;
-        people[1].balance = 0;
+        this.firstPerson = firstPerson;
+        this.secondPerson = secondPerson;
     }
 
-    modifier validateOwner() {
+    function split() public payable {
         require(msg.sender == owner, "You are not the owner!");
-        _;
-    }
-    
-    modifier validateAmount () {
         require(msg.value > 0, "The amount is too low!");
-        _;
-    }
 
-    function split() public payable validateOwner validateAmount {
-        require(people[0].person != address(0), "The address is not set");
-        require(people[1].person != address(0), "The address is not set");
-        
         uint256 halfAmount = (msg.value - (msg.value % 2)) / 2;
 
-        for(uint i = 0; i < 2; i++) {
-            people[i].balance += halfAmount;
-        }
+        balances[firstPerson] = halfAmount;
+        balances[secondPerson] = halfAmount;
 
-        emit LogSplit(people[0].person, halfAmount);
-        emit LogSplit(people[1].person, halfAmount);
+        emit LogSplit(msg.sender, firstPerson, secondPerson, halfAmount);
     }
     
     function withdraw() public {
-        for (uint i=0; i<2; i++) {
-            if (msg.sender == people[i].person && people[i].balance > 0) {
-                uint amount = people[i].balance;
-                people[i].balance = 0;
-                people[i].person.transfer(amount);
-                emit LogWithdraw(people[i].person, amount);
-            }
-        }
+        uint amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+        msg.sender.transfer(amount);
+        emit LogWithdraw(msg.sender, amount);
     }
 }
 
