16616079d89ca1d2631e32ea2ad0988400261850
Patryk Okuniewicz <patryk.okuniewicz@espeo.eu>
2019-02-06 00:57:13 +0100

    Add CR fixes

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,8 +2,6 @@ pragma solidity ^0.4.24;
 
 contract Splitter {
     address public owner;
-    address public firstPerson;
-    address public secondPerson;
 
     mapping(address => uint) public balances;
 
@@ -11,23 +9,22 @@ contract Splitter {
     event LogSplit(address owner, address firstPerson, address secondPerson, uint amount);
     event LogWithdraw(address person, uint amount);
 
-    constructor (address firstPerson, address secondPerson) public {
-        require(firstPerson != address(0), "The address is not set");
-        require(secondPerson != address(0), "The address is not set");
+    constructor () public {
         owner = msg.sender;
         emit LogOwner(owner);
-
-        this.firstPerson = firstPerson;
-        this.secondPerson = secondPerson;
     }
 
-    function split() public payable {
+    function split(address firstPerson, address secondPerson) public payable {
         require(msg.sender == owner, "You are not the owner!");
         require(msg.value > 0, "The amount is too low!");
+        require(firstPerson != address(0), "The address is not set");
+        require(secondPerson != address(0), "The address is not set");
 
         uint256 halfAmount = msg.value / 2;
 
-        balances[owner] += msg.value % 2 == 0 ? 0 : 1;
+        if(msg.value % 2 == 1) {
+            balances[owner] += 1;
+        }
         balances[firstPerson] += halfAmount;
         balances[secondPerson] += halfAmount;
 
@@ -36,9 +33,11 @@ contract Splitter {
 
     function withdraw() public {
         uint amount = balances[msg.sender];
-        balances[msg.sender] = 0;
-        emit LogWithdraw(msg.sender, amount);
-        msg.sender.transfer(amount);
+        if(amount > 0) {
+            balances[msg.sender] = 0;
+            emit LogWithdraw(msg.sender, amount);
+            msg.sender.transfer(amount);
+        }
     }
 }
 
