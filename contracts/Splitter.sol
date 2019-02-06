83b7573c113c97a055653b2c2e8929b892852d3d
Patryk Okuniewicz <patryk.okuniewicz@espeo.eu>
2019-02-06 21:11:08 +0100

    Add CR fixes

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.24;
 
 contract Splitter {
-    address public owner;
 
     mapping(address => uint) public balances;
 
@@ -10,20 +9,19 @@ contract Splitter {
     event LogWithdraw(address person, uint amount);
 
     constructor () public {
-        owner = msg.sender;
-        emit LogOwner(owner);
+        emit LogOwner(msg.sender);
     }
 
     function split(address firstPerson, address secondPerson) public payable {
-        require(msg.sender == owner, "You are not the owner!");
         require(msg.value > 0, "The amount is too low!");
         require(firstPerson != address(0), "The address is not set");
         require(secondPerson != address(0), "The address is not set");
 
         uint256 halfAmount = msg.value / 2;
 
-        if(msg.value % 2 == 1) {
-            balances[owner] += 1;
+        uint remainder = msg.value % 2;
+        if(remainder > 0) {
+            balances[msg.sender] += remainder;
         }
         balances[firstPerson] += halfAmount;
         balances[secondPerson] += halfAmount;
@@ -33,11 +31,10 @@ contract Splitter {
 
     function withdraw() public {
         uint amount = balances[msg.sender];
-        if(amount > 0) {
-            balances[msg.sender] = 0;
-            emit LogWithdraw(msg.sender, amount);
-            msg.sender.transfer(amount);
-        }
+        require(amount > 0, "There is nothing to withdraw!");
+        balances[msg.sender] = 0;
+        emit LogWithdraw(msg.sender, amount);
+        msg.sender.transfer(amount);
     }
 }
 
